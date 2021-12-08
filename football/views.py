7a4f57c0d5bc1e5260f7025e8ee8fa35241851a8
Nur-A-Alam1997 <nuraalamp@gmail.com>
2021-12-08 17:59:59 +0000

    added

--- football/views.py
+++ football/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def welcome(request):
-    return '<h6>welcome to football</h6>'
+    return HttpResponse('<h6>welcome to football</h6>')
+
+
+def template(request):
+    return render(request,'welcome.html',{'name':'Nur'})
